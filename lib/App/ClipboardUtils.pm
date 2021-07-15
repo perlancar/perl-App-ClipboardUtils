37ab5c84a95eb01f6fa9cfb8ac9ceb3715113d9d
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-15 17:38:31 +0700

    Initial commit

---
+++ lib/App/ClipboardUtils.pm
@@ -0,0 +1,15 @@
+package App::ClipboardUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to clipboard
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities related to clipboard:
+
+# INSERT_EXECS_LIST
